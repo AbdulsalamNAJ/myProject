ce2df4861f9f94a2894bd2188f48e58ac9bf5fc4
Abdulsalam Al-Najjar <83532131+AbdulsalamNAJ@users.noreply.github.com>
2022-12-08 21:33:58 +0300

    Add files via upload

---
+++ app-login.js
@@ -0,0 +1,5 @@
+document.getElementById('submit').addEventListener("click", myFun);
+
+function myFun () {
+    window.location.replace("http://www.w3schools.com")
+}
---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('show-1').addEventListener("click", myFun);
+
+function myFun () {
+    window.location.replace("./show_1.html")
+}
